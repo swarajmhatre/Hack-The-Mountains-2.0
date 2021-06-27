1318ed2e19e0d66bb746579833237bf394e89ffc
swarajmhatre <swarajmhatre25@gmail.com>
2021-06-27 12:46:39 +0530

    Hackathon

---
+++ Hack Mountain 2.0/Hack 2.0.ts
@@ -0,0 +1,17 @@
+var id1: HTMLInputElement= <HTMLInputElement>document.getElementById("id1")
+var id2: HTMLInputElement= <HTMLInputElement>document.getElementById("id2")
+var id3: HTMLInputElement= <HTMLInputElement>document.getElementById("id3")
+var id4: HTMLInputElement= <HTMLInputElement>document.getElementById("id4")
+var id5: HTMLInputElement= <HTMLInputElement>document.getElementById("id5")
+var id6: HTMLInputElement= <HTMLInputElement>document.getElementById("id6")
+
+
+function erase(){
+    alert("ur response has been submited")
+    id1.value= ""; 
+    id2.value= ""; 
+    id3.value= ""; 
+    id4.value= ""; 
+    id5.value= ""; 
+    id6.value= ""; 
+}
